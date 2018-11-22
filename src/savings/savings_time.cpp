0f787e17425ef454029933ddee504b46043fb6ec
Leandro Yampolsky <leandroyampolsky@gmail.com>
2018-11-22 05:11:53 -0300

    Add Times notebook. Add new instances.

---
+++ src/savings/savings_time.cpp
@@ -0,0 +1,20 @@
+#include "savings.h"
+#include <iostream>
+#include <chrono>
+
+int main(int argc, char const *argv[])
+{
+    auto cvrp = Cvrp();
+    auto result = cvrp.readInput();
+    if (result != 0) return result;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    auto savings = Savings(cvrp);
+    auto routes = savings.findBestRoutes();
+
+    auto end = chrono::high_resolution_clock::now();
+    cout << chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() << endl;
+
+    return 0;
+}
